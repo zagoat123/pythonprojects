c9950bfa93f10533165ac401e115feb3df898ee2
zagoat123 <devswarup13@gmail.com>
2026-04-07 05:01:12 -0700

    tuple

---
+++ tuple.py
@@ -0,0 +1,18 @@
+student=("dev",12,"black")
+print(student)
+#pack a tuple address
+address=(67,"ZA GOODS","paris","France")
+#unpack a tuple
+HouseNo,AreaName,City,Country=address
+print(f"House number is {HouseNo}")
+print(f"Area name is {AreaName}")
+print(f"City name is {City}")
+print(f"Country name is {Country}")
+#Accessing tuple elements using slicing
+name=("d","e","v","s","w","a","r","u","p")
+print(name[0])
+print(name[1:3])
+print(name[-1])
+print(name[-2:])
+print(student[-1])
+
